6781800cd688e0965fbab7b6d33c9402f88aa439
camilomf <camilo.mino03@inacapmail.cl>
2019-10-23 18:17:06 -0300

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,13 @@
 @endsection
 @section('content')
 
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
 
+        <main role="main" class="inner cover">
+          <h1 class="cover-heading">Bienvenido {{ auth()->user()->name }}</h1>
+        </main>
+      
+      </div>
 
 
     @endsection
